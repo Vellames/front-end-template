630ac451cd58caf130e9091d7ce2f638838dbeaf
Cassiano Vellames <cassiano.vellames@credta.com>
2021-08-28 19:29:38 -0300

    packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react'
+import Button from '@material-ui/core/Button'
 
 function App () {
   return (
     <div className="App">
       <div>Ola</div>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </div>
   )
 }
